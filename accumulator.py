76852e65e33f06da94545bc30d1eb24d4fbc857c
William Gollinger <wgolling@gmail.com>
2019-02-16 21:46:14 -0500

    write and test Accumulator and ListAccumulator

--- accumulator.py
+++ accumulator.py
@@ -22,43 +22,32 @@ class Accumulator:
 
   @classmethod
   def new_entry(cls, total=0):
-    e = cls.Entry(total)
-    return e
+    return cls.Entry(total)
 
   def gain_amount(self, amt):
     self.current_entry.gain_amount(amt)
 
   def split(self):
-    self.current_entry.total += self.current_entry.gain
-    self.entries.append(self.current_entry)
-    self.current_entry = self.new_entry(total)
-
-
-
+    c = self.current_entry
+    c.total += c.gain
+    self.entries.append(c)
+    self.current_entry = self.new_entry(c.total)
 
 
 class ListAccumulator(Accumulator):
   '''
-  Like Accumulator except also keeps lists.
+  Like Accumulator except the Entries also keep lists of the individual gains.
   '''
   class Entry(Accumulator.Entry):
     def __init__(self, total):
       super().__init__(total)
       self.list = []
+    
+    def gain_amount(self, amt):
+        super().gain_amount(amt)
+        self.list.append(amt)
 
-  def gain(self, amt):
-    super().gain(amt)
-    self.list.append(amt)
-
-#Do we need this one?
-class CharacterAccumulator(Accumulator):
-  '''
-  Like Accumulator except also for characters.
-  '''
-  def __init__(self):
-    pass
-
-
+  
 class FunctionAccumulator(Accumulator):
   '''
   Like Accumulator except its value is determined from the values of other accumulators.
--- test_accumulator.py
+++ test_accumulator.py
@@ -3,11 +3,71 @@ import unittest
 
 class TestAccumulator(unittest.TestCase):
 
+  def setUp(self):
+    self.test_instance = Accumulator("Test Key", "Test Label")
+
   def test_constructor(self):
-    pass
+    t = self.test_instance
+    self.assertEqual(t.key  , "Test Key")
+    self.assertEqual(t.label, "Test Label")
+    e = t.current_entry    
+    self.assertEqual(e.gain , 0)
+    self.assertEqual(e.total, 0)
+
+  def test_gain(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    e = t.current_entry
+    self.assertEqual(e.gain, 5)
+    self.assertEqual(e.total, 0)
+
+  def test_split(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    t.split()
+    e0 = t.entries[0]
+    self.assertEqual(e0.gain, 5)
+    self.assertEqual(e0.total, 5)
+    e1 = t.current_entry
+    self.assertEqual(e1.gain, 0)
+    self.assertEqual(e1.total, 5)
+
 
 
 class TestListAccumulator(unittest.TestCase):
 
+  def setUp(self):
+    self.test_instance = ListAccumulator("Test Key", "Test Label")
+
   def test_constructor(self):
-    pass
+    t = self.test_instance
+    self.assertEqual(t.key  , "Test Key")
+    self.assertEqual(t.label, "Test Label")
+    e = t.current_entry
+    self.assertEqual(e.gain , 0)
+    self.assertEqual(e.total, 0)
+    assert(type(e.list) is list and not e.list)
+
+  def test_gain(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    e = t.current_entry
+    self.assertEqual(e.gain, 5)
+    self.assertEqual(e.total, 0)
+    assert(len(e.list) == 1)
+    self.assertEqual(e.list[0], 5)    
+
+  def test_split(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    t.split()
+    e0 = t.entries[0]
+    self.assertEqual(e0.gain, 5)
+    self.assertEqual(e0.total, 5)
+    assert(len(e0.list) == 1)
+    self.assertEqual(e0.list[0], 5)    
+    e1 = t.current_entry
+    self.assertEqual(e1.gain, 0)
+    self.assertEqual(e1.total, 5)
+    assert(len(e1.list) == 0)
+
