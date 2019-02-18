2c3b0bd2ff5290567b378523a4c11e49009625bb
William Gollinger <wgolling@gmail.com>
2019-02-18 17:39:11 -0500

    write and test Current and FunctionAccumulator

--- accumulator.py
+++ accumulator.py
@@ -23,14 +23,18 @@ class Accumulator:
   def new_entry(self, total=0):
     return self.Entry(total)
 
+  def append_current_entry(self):
+    c = self.current_entry
+    self.entries.append(c)
+    self.current_entry = self.new_entry(c.total)
+
   def gain_amount(self, amt):
     self.current_entry.gain_amount(amt)
 
   def split(self):
     c = self.current_entry
     c.total += c.gain
-    self.entries.append(c)
-    self.current_entry = self.new_entry(c.total)
+    self.append_current_entry()
 
 
 class ListAccumulator(Accumulator):
@@ -47,10 +51,48 @@ class ListAccumulator(Accumulator):
         self.list.append(amt)
 
   
+class CurrentAccumulator(Accumulator):
+  '''
+  Like Accumulator but keeps track of the current value of something that can fluctuate.
+  A CurrentAccumulator's current value is set, and its gain is computed upon split().
+  '''
+  def set_current(self, amt):
+    self.current_entry.total = amt
+
+  def split(self):
+    last_current = 0 if not self.entries else self.entries[-1].total
+    c = self.current_entry
+    c.gain = c.total - last_current
+    self.append_current_entry()
+
+
 class FunctionAccumulator(Accumulator):
   '''
   Like Accumulator except its value is determined from the values of other accumulators.
+  Try #1: 
+      - the constructor takes a list if accumulators and a lambda function to apply to those accumulators.
+      - it is assumed that all of the accumulators in the list get split before self.
   '''
-  def __init__(self, accumulators, acc_function):
-    #gotta think about this one. 
-    pass
+  def __init__(self, key, label, accumulators, acc_function):
+    super().__init__(key, label)
+    self.accumulators = accumulators
+    self.function = acc_function
+
+  def split(self):
+    c = self.current_entry
+    c.gain = self.function(self.accumulators)
+    c.total += c.gain
+    self.append_current_entry()
+
+
+
+
+class AccumulatorManager:
+  '''
+  The controller class for the Accumulator model.
+  '''
+  def __init__(self):
+    self.accumulator_categories = dict()
+
+
+
--- test_accumulator.py
+++ test_accumulator.py
@@ -69,3 +69,99 @@ class TestListAccumulator(unittest.TestCase):
     self.assertEqual(e1.gain, 0)
     self.assertEqual(e1.total, 5)
     assert(len(e1.list) == 0)
+
+
+class TestCurrentAccumulator(unittest.TestCase):
+
+  def setUp(self):
+    self.test_instance = CurrentAccumulator("Test Key", "Test Label")
+
+  def test_constructor(self):
+    t = self.test_instance
+    self.assertEqual(t.key  , "Test Key")
+    self.assertEqual(t.label, "Test Label")
+    e = t.current_entry    
+    self.assertEqual(e.gain , 0)
+    self.assertEqual(e.total, 0)
+
+  def test_gain(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    e = t.current_entry
+    self.assertEqual(e.gain, 5)
+    self.assertEqual(e.total, 0)
+
+  def test_set_current(self):
+    t = self.test_instance
+    t.set_current(6)
+    self.assertEqual(t.current_entry.total, 6)
+
+  def test_split(self):
+    t = self.test_instance
+    t.set_current(6)
+    t.split()
+    t.set_current(4)
+    t.split()
+    e0 = t.entries[0]
+    self.assertEqual(e0.gain, 6)
+    self.assertEqual(e0.total, 6)
+    e1 = t.entries[1]
+    self.assertEqual(e1.gain, -2)
+    self.assertEqual(e1.total, 4)
+    #e2 = t.current_entry
+
+
+class TestFunctionAccumulator(unittest.TestCase):
+
+  def setUp(self):
+    self.acc      = Accumulator("acc", "Plain Accumulator")
+    self.list_acc = ListAccumulator("list", "List Accumulator")
+    self.curr_acc = CurrentAccumulator("curr", "Current Accumulator")
+    accs = [self.acc, self.list_acc, self.curr_acc]
+    def acc_function(acc_list):
+      val0 = acc_list[0].entries[-1].gain
+      val1 = acc_list[1].entries[-1].gain 
+      val2 = acc_list[2].entries[-1].gain 
+      return val2 - val0 - val1
+    self.test_instance = FunctionAccumulator("Test Key", "Test Label", accs, acc_function)
+
+  def test_constructor(self):
+    t = self.test_instance
+    self.assertEqual(t.key  , "Test Key")
+    self.assertEqual(t.label, "Test Label")
+    e = t.current_entry    
+    self.assertEqual(e.gain , 0)
+    self.assertEqual(e.total, 0)
+
+  def test_gain(self):
+    t = self.test_instance
+    t.gain_amount(5)
+    e = t.current_entry
+    self.assertEqual(e.gain, 5)
+    self.assertEqual(e.total, 0)
+
+  def test_split(self):
+    t = self.test_instance
+    a = self.acc
+    l = self.list_acc
+    c = self.curr_acc
+    a.gain_amount(5)
+    a.split()
+    l.gain_amount(7)
+    l.split()
+    c.set_current(15)
+    c.split()
+    t.split()
+    a.gain_amount(2)
+    a.split()
+    l.gain_amount(3)
+    l.split()
+    c.set_current(25)
+    c.split()
+    t.split()
+    e0 = t.entries[0]
+    self.assertEqual(e0.gain, 3)
+    self.assertEqual(e0.total, 3)
+    e1 = t.entries[1]
+    self.assertEqual(e1.gain, 5)
+    self.assertEqual(e1.total, 8)
