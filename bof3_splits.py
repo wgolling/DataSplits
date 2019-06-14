fe42433cc2d0105fa34d9b83f9dad5654e149ac3
William Gollinger <wgolling@gmail.com>
2019-06-13 20:29:29 -0400

    change function accumulators in BoF3 Splits to sum accumulators

--- bof3_splits.py
+++ bof3_splits.py
@@ -8,34 +8,28 @@ class BreathOfFire3Splits(AccumulatorManager):
     self.add_character_accumulator("char-ryu", "Ryu")
     # zenny accumulators
     self.add_accumulator_category("zenny", "Zenny")
-    self.add_list_accumulator("zenny-pickups"   , "Pickups")
-    self.add_list_accumulator("zenny-bossdrops" , "Boss Drops")
-    self.add_list_accumulator("zenny-spends"    , "Expenditures")
-    self.add_list_accumulator("zenny-sales"     , "Sales")
-    self.add_current_accumulator("zenny-current", "Current")
-    ## add function accumulator for zenny from enctounters
-    keys_list = ["zenny-pickups", "zenny-bossdrops", "zenny-spends", "zenny-sales", "zenny-current"]
-    def compute_encounters(gain_dict):
-      pickup    = gain_dict["zenny-pickups"]
-      bossdrops = gain_dict["zenny-bossdrops"]
-      spends    = gain_dict["zenny-spends"]
-      sales     = gain_dict["zenny-sales"]
-      current   = gain_dict["zenny-current"]
-      return current - pickup - bossdrops + spends - sales
-    self.add_function_accumulator("zenny-encounters", "Encounters", keys_list, compute_encounters)
+    zenny_pickups   = self.add_list_accumulator("zenny-pickups"   , "Pickups")
+    zenny_bossdrops = self.add_list_accumulator("zenny-bossdrops" , "Boss Drops")
+    zenny_spends    = self.add_list_accumulator("zenny-spends"    , "Expenditures")
+    zenny_sales     = self.add_list_accumulator("zenny-sales"     , "Sales")
+    zenny_current   = self.add_current_accumulator("zenny-current", "Current")
+    ## add sum accumulator for zenny from enctounters
+    self.add_sum_accumulator(
+      "zenny-encounters", 
+      "Encounters", 
+      [zenny_current, zenny_spends],
+      [zenny_pickups, zenny_bossdrops, zenny_sales] )
     # skill ink accumulators
     self.add_accumulator_category("skill_ink", "Skill Ink")
-    self.add_accumulator("skill_ink-pickups",  "Pickups")
-    self.add_accumulator("skill_ink-uses",     "Uses")
-    self.add_accumulator("skill_ink-buys",     "Buys")
-    ## add function accumulator for current skill ink
-    keys_list = ["skill_ink-pickups", "skill_ink-uses", "skill_ink-buys"]
-    def compute_current_skill_ink(gain_dict):
-      pickup  = gain_dict["skill_ink-pickups"]
-      uses    = gain_dict["skill_ink-uses"]
-      buys    = gain_dict["skill_ink-buys"]
-      return pickup + buys - uses
-    self.add_function_accumulator("skill_ink-current", "Current", keys_list, compute_current_skill_ink)
+    sk_pickups  = self.add_accumulator("skill_ink-pickups",  "Pickups")
+    sk_uses     = self.add_accumulator("skill_ink-uses",     "Uses")
+    sk_buys     = self.add_accumulator("skill_ink-buys",     "Buys")
+    ## add sum accumulator for current skill ink
+    self.add_sum_accumulator(
+      "skill_ink-current", 
+      "Current", 
+      [sk_pickups, sk_buys],
+      [sk_uses] )
 
   # Interface functions.
 
